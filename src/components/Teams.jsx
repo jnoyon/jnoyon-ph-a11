cf5f0854aa04f8fbfd8bccae3927297d9dce6510
jnoyon <jihadur51@gmail.com>
2025-01-06 14:09:05 +0600

    Teams Section Added

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 export default function Teams() {
   return (
-    <div className='bg-gradient-to-r from-indigo-100 via-yellow-50 to-pink-100 py-5'>
-        <h1 className='text-center text-2xl font-bold md:text-5xl'> Our Teams </h1>
+    <div className='bg-gradient-to-r from-indigo-100 via-yellow-50 to-pink-100 py-10'>
+        <h1 className='text-center text-2xl font-bold md:text-5xl mb-5'> Our Teams </h1>
+        <div className="flex w-11/12 mx-auto gap-5">
+            <div className="w-1/4 bg-white shadow-md rounded-md flex flex-col items-center py-5">
+                <img src="https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_960_720.png" alt="User" className='rounded-full h-32 w-32 mb-2' />
+                <h2 className='font-bold'> Jihadur Rahman Noyon </h2>
+                <span className='text-sm text-gray-600'> CEO, Stock Room </span>
+                <div className="mt-5 flex gap-1">
+                    <a href="#"> <FaSquareFacebook className="text-3xl text-blue-500" /> </a>
+                    <a href="#"> <FaSquareXTwitter className="text-3xl text-black" /> </a>
+                    <a href="#"> <FaWhatsappSquare className="text-3xl text-green-500" /> </a>
+                    <a href="#"> <FaLinkedin className="text-3xl text-blue-500" /> </a>
+                </div>
+            </div>
+            <div className="w-1/4 bg-white shadow-md rounded-md flex flex-col items-center py-5">
+                <img src="https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_960_720.png" alt="User" className='rounded-full h-32 w-32 mb-2' />
+                <h2 className='font-bold'> Jihadur Rahman Noyon </h2>
+                <span className='text-sm text-gray-600'> CEO, Stock Room </span>
+                <div className="mt-5 flex gap-1">
+                    <a href="#"> <FaSquareFacebook className="text-3xl text-blue-500" /> </a>
+                    <a href="#"> <FaSquareXTwitter className="text-3xl text-black" /> </a>
+                    <a href="#"> <FaWhatsappSquare className="text-3xl text-green-500" /> </a>
+                    <a href="#"> <FaLinkedin className="text-3xl text-blue-500" /> </a>
+                </div>
+            </div>
+            <div className="w-1/4 bg-white shadow-md rounded-md flex flex-col items-center py-5">
+                <img src="https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_960_720.png" alt="User" className='rounded-full h-32 w-32 mb-2' />
+                <h2 className='font-bold'> Jihadur Rahman Noyon </h2>
+                <span className='text-sm text-gray-600'> CEO, Stock Room </span>
+                <div className="mt-5 flex gap-1">
+                    <a href="#"> <FaSquareFacebook className="text-3xl text-blue-500" /> </a>
+                    <a href="#"> <FaSquareXTwitter className="text-3xl text-black" /> </a>
+                    <a href="#"> <FaWhatsappSquare className="text-3xl text-green-500" /> </a>
+                    <a href="#"> <FaLinkedin className="text-3xl text-blue-500" /> </a>
+                </div>
+            </div>
+            <div className="w-1/4 bg-white shadow-md rounded-md flex flex-col items-center py-5">
+                <img src="https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_960_720.png" alt="User" className='rounded-full h-32 w-32 mb-2' />
+                <h2 className='font-bold'> Jihadur Rahman Noyon </h2>
+                <span className='text-sm text-gray-600'> CEO, Stock Room </span>
+                <div className="mt-5 flex gap-1">
+                    <a href="#"> <FaSquareFacebook className="text-3xl text-blue-500" /> </a>
+                    <a href="#"> <FaSquareXTwitter className="text-3xl text-black" /> </a>
+                    <a href="#"> <FaWhatsappSquare className="text-3xl text-green-500" /> </a>
+                    <a href="#"> <FaLinkedin className="text-3xl text-blue-500" /> </a>
+                </div>
+            </div>
+        </div>
+        <div className="text-center mt-5">
+            <Link to='/' className="btn btn-wide btn-primary"> Learn More </Link>
+        </div>
+        
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
         <Banner></Banner>
-        <Map></Map>
+        {/* <Map></Map> */}
         <Teams></Teams>
     </div>
   )
